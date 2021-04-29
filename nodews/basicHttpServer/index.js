0a17ea685bbac4119e4746e1db9d078c1d4681a9
Sparsh17 <sparshgagneja1999@gmail.com>
2021-04-29 13:14:07 +0530

    Link multiple pages

--- nodews/basicHttpServer/index.js
+++ nodews/basicHttpServer/index.js
@@ -1,20 +1,43 @@
 const http = require('http');
 const port = 8000;
 const fs = require('fs');  // fs is File System
+const { connected } = require('process');
 
 function requestHandler(req,res) {
     console.log(req.url);
     res.writeHead(200,{'content-type':'text/html'});
 
-    fs.readFile('./index.html', function(err, data){
-        if (err) {
-            console.log('error',err);
+    let filePath;
+
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+
+        default:
+            filePath = './404.html'
+    }
+
+    fs.readFile(filePath,function(err,data){
+        if(err){
+            console.log('Error',err);
             return res.end('<h1>Error</h1>')
         }
-
         return res.end(data);
+    })
 
-    });
+    // fs.readFile('./index.html', function(err, data){
+    //     if (err) {
+    //         console.log('error',err);
+    //         return res.end('<h1>Error</h1>')
+    //     }
+
+    //     return res.end(data);
+
+    // });
 
     // res.end('<h1 style="color:red">Hello!</h1>');
 }
@@ -27,5 +50,5 @@ server.listen(port, function(err){
         return;
     }
 
-    console.log("Server is up and running on port: ",port);
+    console.log("Server is running on port: ",port);
 });
