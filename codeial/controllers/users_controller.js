61ff4b858c7906c43be217ce46c8090418a79344
Sparsh17 <sparshgagneja1999@gmail.com>
2021-06-01 23:29:08 +0530

    Make authentication

--- codeial/controllers/users_controller.js
+++ codeial/controllers/users_controller.js
@@ -59,36 +59,47 @@ module.exports.create = function(req, res){
     })
 };
 
+// // sign in and create session for user
+// module.exports.createSession = function(req, res){
+//     // steps to authenticate
+//     // find the user
+//     User.findOne({email: req.body.email}, function(err,user){
+//         if(err){
+//             console.log('Error in finding user in signing up');
+//             return;
+//         }
+
+//         // handle user found
+//         if(user){
+
+//             // handle password which dont match
+//             if (user.password != req.body.password){
+//                 return res.redirect('back');
+//             }
+//             //handle session creation
+//             res.cookie('user_id', user.id);
+//             return res.redirect('/users/profile');
+
+//         }else{
+//             //handle user not found
+//             return res.redirect('back');
+//         }
+//     });
+// };
+
+// // render Sign Out
+// module.exports.deleteSession = function(req, res){
+//     res.clearCookie('user_id');
+//     return res.redirect('/users/sign-in');
+// };
+
+
+
+
+
+
+
 // sign in and create session for user
 module.exports.createSession = function(req, res){
-    // steps to authenticate
-    // find the user
-    User.findOne({email: req.body.email}, function(err,user){
-        if(err){
-            console.log('Error in finding user in signing up');
-            return;
-        }
-
-        // handle user found
-        if(user){
-
-            // handle password which dont match
-            if (user.password != req.body.password){
-                return res.redirect('back');
-            }
-            //handle session creation
-            res.cookie('user_id', user.id);
-            return res.redirect('/users/profile');
-
-        }else{
-            //handle user not found
-            return res.redirect('back');
-        }
-    });
-};
-
-// render Sign Out
-module.exports.deleteSession = function(req, res){
-    res.clearCookie('user_id');
-    return res.redirect('/users/sign-in');
+    return res.redirect('/');
 };
