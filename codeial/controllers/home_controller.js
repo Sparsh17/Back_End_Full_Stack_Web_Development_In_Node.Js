78ae8c75d3e3255eaca7d30b8ea867f31069044f
Sparsh17 <sparshgagneja1999@gmail.com>
2021-06-12 18:21:24 +0530

    Added Delete and Update

--- codeial/controllers/home_controller.js
+++ codeial/controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user')
 
 module.exports.home = function(req,res){
     
@@ -19,10 +20,15 @@ module.exports.home = function(req,res){
         }
     })
     .exec(function(err,posts){
-        return res.render('home', {
-            title: "Home",
-            posts: posts
-        });
+
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Home",
+                posts: posts,
+                all_users: users
+            });
+        })
+        
     });
     
 };
